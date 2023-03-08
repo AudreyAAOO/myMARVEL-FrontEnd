debf874cb4c10d3f97ca9fcf5e270cd7a3ec238a
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-03-08 22:14:28 +0100

    ✨ Character > add localStorage

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -15,20 +15,26 @@ const Characters = () => {
 	const [data, setData] = useState();
 	const [isLoading, setIsLoading] = useState(true);
 	const [searchC, setSearchC] = useState("");
+
+	//? gestion pagination
 	const [skip, setSkip] = useState(0);
 	const [limit] = useState(100);
 	const [isLastPage, setIsLastPage] = useState(false);
-	const [pinsChar, setPinsChar] = useState([]);
-	const [isFavorite, setIsFavorite] = useState(false)
+
+	//? gestion des favoris
+	const [pinsChar, setPinsChar] = useState(
+		localStorage.getItem('pins')               // vérifier s'il y a qqch le storage
+			? JSON.parse(localStorage.getItem('pins')) // vérifier qu'il existe des datas avant de parser sinon erreur
+			: [])
+
+
+	const [isFavorite, setIsFavorite] = useState(false)   // ds le use effet vérifier si le character est ds les favoris
+
+	//! test pr afficher les cards
+
+
 
 	// const handlePins = () => {
-	// 	const fav = { id: character._id, name: character.name };
-	// 	console.log("fav: ", fav);
-	// 	const copy = [...pinsChar];
-	// 	copy.push(fav);
-	// 	console.log("copy", copy);
-	// 	setPinsChar(copy);
-	// 	localStorage.setItem('pins', JSON.stringify(copy));
 	// };
 
 	useEffect(() => {
@@ -43,7 +49,7 @@ const Characters = () => {
 			}
 		};
 		fetchData();
-	}, [searchC, skip, limit]);
+	}, [searchC, skip, limit]); // = surveiller ce qu'il yu ds le tableau de dépendance, si ces variables changent, il faut se réexecuter
 
 	let displayImg = (character) => {
 		// eslint-disable-next-line
@@ -84,9 +90,16 @@ const Characters = () => {
 			</div>
 		</div>
 
+
 		<div className="container">
 			{data.results.map((character, i) => {
 				return (
+					// faire un composant character card et créer le state isFavorite dedans pr qu'il s'applique à chaque tour à chaque character (et non au 100 d'un coup.)
+					// Penser au use effect pour vérifier si le character est déjà ds les favoris (bien mettre if et ELSE)
+					// + surveiller l'état de use isfavorite ds le tableau de dépendance de useEffect (sinon erreur)
+					// faire une requête à notre backend pour aller chercher les infos sur les personnages favoris
+					// créer une route dans le backend qui va faire une requête à l'api pour aller chercher le character dans l'id est déjà ds les favoris/storage et qu'on veut réafficher.
+
 					<div key={i} className="charactersCard">
 
 						<article key={character._id}>
@@ -105,23 +118,22 @@ const Characters = () => {
 									icon={["far", "heart"]}
 									// onClick={(e) => handlePins(e)}
 									onClick={() => {
-										const fav = { id: character._id, name: character.name };
-										console.log("fav: ", fav);
+										const charId = character._id;
 										const copy = [...pinsChar];
-										copy.push(fav);
-										console.log("copy", copy);
-										setPinsChar(copy);
-										localStorage.setItem('pins', JSON.stringify(copy));
-										setIsFavorite(!isFavorite);
-										if (!isFavorite) {
-											// localStorage.removeItem('pins');
-											const copy = [...pinsChar];
-											copy.pop(fav);
-											console.log("copy", copy);
-											setPinsChar(copy);
+										const result = copy.indexOf(charId)
+										if (result === -1) { //* si id n'est pas dans le tab des favoris
+											copy.push(charId);
+											localStorage.setItem('pins', JSON.stringify(copy));
+											console.log("if result: ", result, "copy: ", copy);
+										} else {
+											copy.splice(result, 1)
+											localStorage.removeItem(result);
+											console.log("else result: ", result, "copy: ", copy);
 										}
-									}}
-
+										setPinsChar(copy);
+										console.log("PinsChar: ", pinsChar, "copy: ", copy);
+									}
+									}
 								/>
 							</div>
 						</article>
